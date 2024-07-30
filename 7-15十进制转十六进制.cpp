49aec9ecb7acbe2b687a152fcfe9d900b185720f
susu-ctrl <1156510350@qq.com>
2024-07-30 16:26:38 +0800

    Create 7-15十进制转十六进制.cpp

---
+++ 7-15十进制转十六进制.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS  
+#include <stdio.h>  
+  
+int main() {  
+    int n;
+  
+    // 读取输入
+    scanf("%d", &n);  
+  
+    // 输出结果
+    printf("%X", n);  
+  
+    return 0;  
+}
